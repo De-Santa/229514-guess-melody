e7336ef097e198f8e7b158f4d073e8d38c375759
German Topalov <gaijin773@yandex.ru>
2017-10-31 02:55:03 +0700

    count time

--- js/components/game-screen/game-screen.js
+++ js/components/game-screen/game-screen.js
@@ -3,24 +3,41 @@ import GameScreenView from './game-screen-view';
 import resultScreen from '../result-screen/result-screen';
 import gameLevels from '../../data/game-levels';
 import gameData from '../../data/game-data';
+import Timer from '../../utils/timer';
 
 const gameScreen = (gameState) => {
-  const gameScreenView = new GameScreenView(gameState);
+  let currentGameState = gameState;
+  const gameScreenView = new GameScreenView(currentGameState);
+  const timer = new Timer(currentGameState.time);
+
+  const intervalId = setInterval(() => {
+    timer.tick();
+    currentGameState = Object.assign({}, currentGameState, {time: timer.value});
+    if (timer.value > 0) {
+      gameScreenView.updateTimer(timer.value);
+    } else {
+      clearTimeout(intervalId);
+      switchScreen(resultScreen(currentGameState));
+    }
+  }, 1000);
 
   gameScreenView.onAnswer = (answer) => {
-    const currentGameState = Object.assign({}, gameState);
+    currentGameState = Object.assign({}, currentGameState);
 
     currentGameState.answers.push(answer);
     if (!answer.isCorrect) {
       currentGameState.mistakes += 1;
     }
-    console.log(currentGameState.mistakes, gameData.MAX_MISTAKES);
+
     if (currentGameState.currentLevel === gameLevels.length - 1 || currentGameState.mistakes > gameData.MAX_MISTAKES) {
+      clearTimeout(intervalId);
       switchScreen(resultScreen(currentGameState));
     } else {
+      clearTimeout(intervalId);
       currentGameState.currentLevel += 1;
       switchScreen(gameScreen(currentGameState));
     }
+
   };
 
   return gameScreenView;
--- js/components/timer.js
+++ js/components/timer.js
@@ -1,4 +1,4 @@
-export default () => {
+export default (seconds) => {
   return `
     <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
       <circle
@@ -7,9 +7,9 @@ export default () => {
         style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
 
       <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-        <span class="timer-value-mins">05</span><!--
+        <span class="timer-value-mins"></span><!--
         --><span class="timer-value-dots">:</span><!--
-        --><span class="timer-value-secs">00</span>
+        --><span class="timer-value-secs"></span>
       </div>
     </svg>
   `.trim();
